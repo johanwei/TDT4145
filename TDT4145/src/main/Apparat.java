c1802467e1aa21786308961c12b637af868d246c
johanwei <you@example.com>
2018-03-10 14:02:45 +0100

    Finished Apparat.java, started on Ovelse.java

--- TDT4145/src/main/Apparat.java
+++ TDT4145/src/main/Apparat.java
@@ -3,22 +3,31 @@ package main;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class Apparat extends Connector {
 	
-	@FXML private TextField apparatNavn;
-	@FXML private TextArea beskrivelse;
-
 	public Apparat() {
 		Connector.connect();
 	}
 	
-	public void leggTilApparat() throws SQLException {
+	public void leggTilApparat(TextField apparatNavn, TextArea beskrivelse) throws SQLException {
 		 Statement stmt = conn.createStatement();
 		 String sql = String.format("INSERT INTO `Apparat` VALUES ('%s', '%s')", apparatNavn.getText(), beskrivelse.getText());
 		 stmt.executeUpdate(sql);
 	}	
+	
+	public void test() throws SQLException {
+	Statement stmt = conn.createStatement();
+	String sql = String.format("INSERT INTO `Trening` VALUES (NULL, '%s', '%s', '%s', '%s', '%s')", "2010-12-12", "12:12:12", "12", "11", "12");
+	//String sql = String.format("INSERT INTO `Trening` VALUES (NULL, '2018-03-14', '06:00:00', '32', '23', '23')");
+	stmt.executeUpdate(sql);
+	 System.out.println("Trening lagt til!");
+	}
+	
+	public static void main(String[] args) throws SQLException {
+		Apparat a = new Apparat();
+		a.test();
+	}
 }
---
+++ TDT4145/src/main/Ovelse.java
@@ -0,0 +1,77 @@
+package main;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Ovelse extends Connector {
+
+	public Ovelse() {
+		Connector.connect();
+	}
+	
+	public void ovelseGruppe(TextField ovelseGruppeNavn) throws SQLException {
+		Statement stmt = conn.createStatement();
+		String sql = String.format("INSERT INTO `OvelseGruppe` VALUES ('%s')", ovelseGruppeNavn);
+		stmt.executeUpdate(sql);
+	}
+	
+	
+	public void ovelseMedApparat(TextField ovelseNavn, TextField apparatNavn, TextField ovelseGruppeNavn, TextArea beskrivelse) throws SQLException{
+		if (notExists("Apparat", "ApparatNavn", apparatNavn.getText())){
+			new Apparat().leggTilApparat(apparatNavn, beskrivelse);
+		}
+		if (notExists("OvelseGruppe", "OvelseGruppeNavn", ovelseGruppeNavn.getText())){
+			ovelseGruppe(ovelseGruppeNavn);
+		}
+		if (notExists("Ovelse", "OvelseNavn", ovelseNavn.getText())) {
+			Statement stmt = conn.createStatement();
+			String sql = String.format("INSERT INTO Ovelse VALUES ('%s', '%s')", ovelseNavn.getText(), ovelseGruppeNavn.getText());
+			stmt.executeUpdate(sql);
+		} else {
+			throw new IllegalArgumentException("Denne øvelsen eksisterer allerede!");
+		}
+		Statement stmt1 = conn.createStatement();
+		String sql1 = String.format("INSERT INTO OvelseMedApparat VALUES ('%s', '%s', '%s')", ovelseNavn.getText(), apparatNavn.getText(), ovelseGruppeNavn.getText());
+		stmt1.executeUpdate(sql1);
+	}
+	
+	
+	public void ovelseUtenApparat(TextField ovelseNavn, TextArea beskrivelse, TextField ovelseGruppeNavn) throws SQLException {
+		if (notExists("OvelseGruppe", "OvelseGruppeNavn", ovelseGruppeNavn.getText())){
+			ovelseGruppe(ovelseGruppeNavn);
+		}
+		if (notExists("Ovelse", "OvelseNavn", ovelseNavn.getText())) {
+			Statement stmt = conn.createStatement();
+			String sql = String.format("INSERT INTO Ovelse VALUES ('%s', '%s')", ovelseNavn.getText(), ovelseGruppeNavn.getText());
+			stmt.executeUpdate(sql);
+		} else {
+			throw new IllegalArgumentException("Denne øvelsen eksisterer allerede!");
+		}
+		Statement stmt1 = conn.createStatement();
+		String sql1 = String.format("INSERT INTO OvelseUtenApparat VALUES ('%s', '%s', '%s')", ovelseNavn.getText(), beskrivelse.getText(), ovelseGruppeNavn.getText());
+		stmt1.executeUpdate(sql1);
+	}
+	
+	
+	public boolean notExists(String table, String attribute, String name) throws SQLException {
+		String query = String.format("SELECT * FROM %s WHERE %s='%s'", table, attribute, name);
+		PreparedStatement stmt = Connector.conn.prepareStatement(query);
+		System.out.println(query);
+		ResultSet rs = stmt.executeQuery();
+		if(rs.next()) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	public static void main(String[] args) throws SQLException {
+		//Ovelse a = new Ovelse();
+		//a.ovelseUtenApparat("Knebøy", "Det funker!", "Utholdenhet");
+	}
+}
